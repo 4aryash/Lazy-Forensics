8438d81a37a25457d0097dcdb5347a2ea681b255
Aaryash <31902160+DirtyVibe@users.noreply.github.com>
2020-09-22 21:36:35 +0530

    22-09-2020

--- run.sh
+++ run.sh
@@ -1,46 +1,72 @@
 #!/bin/bash
+#made by DirtyV!BE
 
 printf "\n"
 echo "      _____ ____ __   __    _____  ___   ____ _____ __   _ _____ ___ ____ _____" | lolcat -a -s 40
 echo " |    |___|  __/   \_/      |____ |   | |___/ |____ | \  | |____  |  |    |___ " | lolcat -a -s 40
 echo " |___ |   | /___    |       |     |___| |  \_ |____ |  \_| ____| _|_ |___ ____|" | lolcat -a -s 40
-printf "\n-------------------- Ｒｅｄｕｎｄａｎｃｙ Ａｕｔｏｍａｔｅｄ -------------------\n" | lolcat -a -s 20
+printf "\n-------------------- Ｒｅｐｅｔｉｔｉｏｎ Ａｕｔｏｍａｔｅｄ -------------------\n" | lolcat -a -s 20
 
-printf "\nThe strings greater than 8 Characters in length in your file are:\n\n"
-strings $1 | grep -x '.\{9\}'
+printf "\nThe strings greater than $2 Characters in length in your file are:\n\n"
+strings $1 -n $2
 printf "\nAlthough it is still advisable to have a look at the strings manually once.\n\n\n"
 
-list="Binwalk Exiftool Hexdump Zsteg HiddenText Foremost"
+exit-program()
+{
+	echo -e "\x1b[31m \x1b[1m \x1b[5m \x1b[8m"
+	echo -e "Exited Successfully!" && kill -9 $BASHPID
+	#exit
+}
+
+list="Binwalk Exiftool Hexdump Zsteg HiddenText(Snow) Foremost Quit"
 
 echo -e "\x1b[36m ᔕEᒪEᑕT ᗩᑎ OᑭTIOᑎ"
+echo -e "\x1b[33m To know more about the tools, try 'apropos tool-name' or 'man tool-name'"
 
 select option in $list;
 do
 	if [ $REPLY = 1 ];
-	then	binwalk -e $1
+	then	echo ""
+		printf "Binwalk" && binwalk -e $1 | tee -a log.txt
 		echo "Check pwd for extracted files."
-		printf "\n---------------END---------------\n"
-		echo "Press Enter to see the list"
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+		
 	elif [ $REPLY = 2 ];
-	then	exiftool $1
-		printf "\n---------------END---------------\n"
-		echo "Press Enter to see the list"
+	then	echo ""
+		printf "Exiftool\n\n" && exiftool $1 | tee -a log.txt
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+
 	elif [ $REPLY = 3 ];
-	then	xxd $1
-		printf "\n---------------END---------------\n"
-		echo "Press Enter to see the list"
+	then	echo ""
+		printf "Hexdump\n\n" && xxd $1 | tee -a log.txt
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+		
 	elif [ $REPLY = 4 ];
-	then	zsteg -a $1
-		printf "\n---------------END---------------\n"
-		echo "Press Enter to see the list"
+	then	echo ""
+		printf "Zsteg\n\n" && zsteg -a $1 | tee -a log.txt
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+		
 	elif [ $REPLY = 5 ];
-	then	sed -n l $1
-		printf "\n---------------END---------------\n"
-		echo "Press Enter to see the list"
+	then	echo ""
+		printf "Snow\n\n" && sed -n l $1 | tee -a log.txt
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+		
 	elif [ $REPLY = 6 ];
-	then	foremost -i $1
-		printf "\nCheck output directory for audit.txt"
-		printf "---------------END---------------\n"
-		echo "Press Enter to see the list"
+	then	echo ""
+		printf "Foremost\n\n" && foremost -i $1 | tee -a log.txt
+		sleep 2
+		printf "\nCheck output directory for audit.txt\n"
+		printf "\n---------------END---------------\n\n"
+		echo "Press Enter to see the list again"
+		
+	elif [ $REPLY = 7 ];
+	then
+		printf "\n\nNOTE : Make sure to delete the previous logs before performing the analysis on a new file.\n\n"
+		exit-program
 	fi
 done
