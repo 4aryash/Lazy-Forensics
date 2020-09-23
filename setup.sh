96a0ff4af69f80841b30c7e8603f93eda8c10194
Aaryash <31902160+DirtyVibe@users.noreply.github.com>
2020-09-23 14:10:19 +0530

    23-09-2020

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #Tools required to run the script.
-sudo apt-get install -y binwalk
-sudo apt-get install -y exiftool
-sudo apt-get install -y xxd
-git clone "https://github.com/zed-0xff/zsteg.git"
+sudo apt-get install -y binwalk         #tool for searching binary images for embedded files and executable code
+sudo apt-get install -y exiftool        #Read and write meta information in files
+sudo apt-get install -y xxd             #creates  a  hex  dump of a given file or standard input
+git clone "https://github.com/zed-0xff/zsteg.git"       #detect stegano-hidden data in PNG & BMP files
 sudo mv zsteg/bin/zsteg* /usr/local/bin && rm -rf zsteg
-sudo apt-get install -y foremost
-sudo apt-get install lolcat
+sudo apt-get install -y foremost        #Recover files using their headers, footers, and data structures
+sudo apt-get install lolcat             #rainbow coloring effect for text console display
